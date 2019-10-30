e2459387273c1b812840ceec3057a8c611c2871c
David Ramiro Barragan <dramirobarragan@gmail.com>
2019-10-30 21:04:23 +0100

    configure prettier

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,33 @@ import PostList from './post/PostList';
 import CreatePost from './post/CreatePost';
 
 const defaultPosts = [
-  {
-    title: 'React Hooks',
-    content: 'The greatest thing since sliced bread!',
-    author: 'Daniel Bugl'
-  },
-  {
-    title: 'Using React Fragments',
-    content: 'Keeping the DOM tree clean!',
-    author: 'Daniel Bugl'
-  }
+    {
+        title: 'React Hooks',
+        content: 'The greatest thing since sliced bread!',
+        author: 'Daniel Bugl',
+    },
+    {
+        title: 'Using React Fragments',
+        content: 'Keeping the DOM tree clean!',
+        author: 'Daniel Bugl',
+    },
 ];
 
 function App() {
-  const [user, setUser] = useState('');
-  const [posts, setPosts] = useState(defaultPosts);
-  return (
-    <div style={{ padding: 8 }}>
-      <UserBar user={user} setUser={setUser} />
-      <br />
-      {user && <CreatePost user={user} setPosts={setPosts} posts={posts} />}
-      <br />
-      <hr />
-      <PostList posts={posts} />
-    </div>
-  );
+    const [user, setUser] = useState('');
+    const [posts, setPosts] = useState(defaultPosts);
+    return (
+        <div style={{ padding: 8 }}>
+            <UserBar user={user} setUser={setUser} />
+            <br />
+            {user && (
+                <CreatePost user={user} setPosts={setPosts} posts={posts} />
+            )}
+            <br />
+            <hr />
+            <PostList posts={posts} />
+        </div>
+    );
 }
 
 export default App;
--- src/user/Login.jsx
+++ src/user/Login.jsx
@@ -1,34 +1,38 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-export default function Login({setUser}) {
-  const [userName, setUserName] = useState('');
+export default function Login({ setUser }) {
+    const [userName, setUserName] = useState('');
 
-  function handleUserName(evt) {
-    setUserName(evt.target.value);
-  }
+    function handleUserName(evt) {
+        setUserName(evt.target.value);
+    }
 
-  function handleSubmit(evt) {
-    evt.preventDefault();
-    setUser(userName);
-  }
+    function handleSubmit(evt) {
+        evt.preventDefault();
+        setUser(userName);
+    }
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor='login-username'>Username:</label>
-      <input
-        type='text'
-        name='login-username'
-        id='loginusername'
-        onChange={handleUserName}
-      />
-      <label htmlFor='login-password'>Password:</label>
-      <input type='password' name='login-password' id='loginpassword'/>
-      <input type='submit' value='Login' disabled={userName.length === 0}/>
-    </form>
-  );
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="login-username">Username:</label>
+            <input
+                type="text"
+                name="login-username"
+                id="loginusername"
+                onChange={handleUserName}
+            />
+            <label htmlFor="login-password">Password:</label>
+            <input type="password" name="login-password" id="loginpassword" />
+            <input
+                type="submit"
+                value="Login"
+                disabled={userName.length === 0}
+            />
+        </form>
+    );
 }
 
 Login.prototype = {
-  setUser: PropTypes.func.isRequired,
+    setUser: PropTypes.func.isRequired,
 };
